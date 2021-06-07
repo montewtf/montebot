8b4c2f9724c3f8f5b899bdbb15cb321df6b4b076
montewtf <72993336+montewtf@users.noreply.github.com>
2021-06-06 23:20:32 -0400

    error catch for missing commands
    feat custom code for when my friend makes a typo

--- main.py
+++ main.py
@@ -15,6 +15,16 @@ bot.load_extension("fight")
 def are_u_monte(ctx):
     return ctx.author.id == 125782351065251840
 
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CommandNotFound):
+        print(str(ctx.author)+" said !"+str(error)[9:str(error).rfind("\"")])
+        if ctx.author.id == 469630889630892052:
+            await ctx.send("Spell Better")
+        else:
+            await ctx.send("Invalid Command")
+    else: raise error
+
 @bot.command(hidden=True)
 @commands.check(are_u_monte)
 async def load(ctx, extension):
