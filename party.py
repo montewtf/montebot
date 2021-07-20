03f122375ecb462445b793e333d71774b16e5079
montewtf <72993336+montewtf@users.noreply.github.com>
2021-07-20 14:13:27 -0400

    rewrote wild

--- party.py
+++ party.py
@@ -45,7 +45,7 @@ class Pokemon(commands.Cog):
             i+=1
         await ctx.send(embed=embed)
         with open("json/parties/"+str(ctx.author.id)+".json", "w") as f_obj:
-            json.dump(party.recon(), f_obj, indent=4)
+            json.dump(party.export(), f_obj, indent=4)
 
     @commands.command(brief="Checks your Pokemon's summary", description="Checks your Pokemon's summary", help="slot has to be 1-6 and has to have a Pokemon in it. Can be used during battles.")
     async def summary(self, ctx, slot:int):
@@ -83,34 +83,6 @@ class Pokemon(commands.Cog):
         await ctx.send(file=file, embed=embed)
         await ctx.send(embed=embed2)
     
-    def are_u_monte(ctx):
-        return ctx.author.id == 125782351065251840
-    
-    @commands.command(hidden=True)
-    @commands.check(are_u_monte)
-    async def add(self, ctx, species, level, name=None):
-        with open("json/pokedex.json") as f_obj:
-            dex = json.load(f_obj)
-        i=0
-        while i<151:
-            poke = dex[i]
-            if species.capitalize() == poke["name"]:
-             break
-            i+=1
-        poke = pokeparty.pokemon(species=poke, level=level, name=name)
-        poke.catch=None
-        poke.run=None
-        poke.xpyield=None
-        with open("json/parties/"+str(ctx.author.id)+".json") as f_obj:
-            party = pokeparty.party(json.load(f_obj))
-        add=party.add(poke)
-        if not add:
-            await ctx.send("Your party is full")
-            return
-        await ctx.send("Pokemon added to slot: "+str(add))     
-        with open("json/parties/"+str(ctx.author.id)+".json", "w") as f_obj:
-            json.dump(party.recon(), f_obj, indent=4)
-            
     @commands.command(brief="Releases Pokemon", description="Releases Pokemon", help="Slot has to be 1-6 and have a pokemon in it. Gives you a chance to confirm before it releases. Can't be used in battle")
     @commands.check(in_fight)
     async def release(self, ctx, slot):
@@ -129,7 +101,7 @@ class Pokemon(commands.Cog):
         if answer.content.lower() == "y" or answer.content.lower() == "yes":
             await ctx.send("Pokemon released")
             with open("json/parties/"+str(ctx.author.id)+".json", "w") as f_obj:
-                json.dump(party.recon(), f_obj, indent=4)
+                json.dump(party.export(), f_obj, indent=4)
         elif answer.content.lower() == "n" or answer.content.lower() == "no":
             await ctx.send("Release cancelled")
         else:
@@ -146,7 +118,7 @@ class Pokemon(commands.Cog):
         if int(slot2)>int(slot1): await ctx.send("Pokemon "+slot1+" & "+slot2+" have been swapped")
         else: await ctx.send("Pokemon "+slot2+" & "+slot1+" have been swapped")
         with open("json/parties/"+str(ctx.author.id)+".json", "w") as f_obj:
-            json.dump(party.recon(), f_obj, indent=4)
+            json.dump(party.export(), f_obj, indent=4)
 
     @commands.command(brief="Picks a starter", description="Picks a starter", help="Starter has to be one of the kanto starters. Name is optional nickname. Can't be used with pokemon in your party")
     async def starter(self, ctx, starter, name=None):
@@ -166,10 +138,15 @@ class Pokemon(commands.Cog):
                 poke.catch = None
                 poke.run = None
                 poke.xpyield = None
-                tdata = {"order": [1,2,3,4,5,6]}
+                tdata = {"order": [1,2,3,4,5,6],
+                         "level": 5,
+                         "box": 1}
                 party = {"0":tdata, "1":poke.export(),"2":None,"3":None,"4":None,"5":None,"6":None,}
+                pc = pokeparty.pc()
                 with open("json/parties/"+str(ctx.author.id)+".json", "w") as f_obj:
                     json.dump(party, f_obj, indent=4)
+                with open("json/pc/"+str(ctx.author.id)+".json", "w") as f_obj:
+                    json.dump(pc.export(), f_obj)
                 await ctx.send("You have obtained "+str(starter).capitalize()+"!")
             else:
                 await ctx.send("Invalid starter")
@@ -183,47 +160,64 @@ class Pokemon(commands.Cog):
         while i<7:
             if party.get(i)!=None:
                 poke = party.get(i)
+                poke.resetMod()
                 list1=[poke.move1,poke.move2,poke.move3,poke.move4]
                 for move in list1:
                     if move!=None:
                         move.curpp=move.maxpp
                 poke.move1,poke.move2,poke.move3,poke.move4 = list1
-                poke.status = None
+                poke.status = {}
                 poke.curhp =  poke.hp
                 party.setp(i,poke)
             i+=1
         await ctx.send("We hope to see you again")
         with open("json/parties/"+str(ctx.author.id)+".json", "w") as f_obj:
-            json.dump(party.recon(), f_obj, indent=4)
+            json.dump(party.export(), f_obj, indent=4)
             
     @commands.command(brief="Generates a wild pokemon battle", description="Generates a wild pokemon battle", help="Wild pokemon can be any of 151 and a random level 1-100")
-    async def wild(self, ctx):
+    async def wild(self, ctx, area=None):
         with open("json/parties/"+str(ctx.author.id)+".json") as f_obj:
             party = pokeparty.party(json.load(f_obj))
         checker = False
         if party.p7==None:
             checker = True
             i=2
-            while party.p1.status=="fainted":
+            while "fainted" in party.p1.status:
                 party.swap(1,i)
                 if i==7 or party.p1==None:
                     return await ctx.send("All your pokemon are fainted")
                 i+=1
+            with open("json/wild.json") as f_obj:
+                spawns = json.load(f_obj)
+            newspawns = {}
+            string = ""
+            embed=discord.Embed(title="Available Areas", color=discord.Color.blue())
+            for key in spawns:
+                if int(key) <= party.level:
+                    for key2 in spawns[key]:
+                        newspawns[key2] = spawns[key][key2]
+                        string += key2+"\n"
+                    embed.add_field(name=key, value=string, inline=False)
+                    string = ""
+                else: break
+            if area == None:
+                def yes(m):
+                    return m.author == ctx.author
+                await ctx.send("Select an area", embed=embed)
+                try:
+                    answer = await self.bot.wait_for("message", check=yes, timeout=30.0)
+                except asyncio.TimeoutError:
+                    return await ctx.send("Timed out")
+                area = answer.content
+            if area not in newspawns:
+                return await ctx.send("Invalid Area")
+            else:
+                area = newspawns[area]
+            choice = random.choices(area[0], area[1])[0]
             with open("json/pokedex.json") as f_obj:
                 dex = json.load(f_obj)
-            poke=dex[random.randint(0, 149)]
-            lvltot=0
-            pokect=0
-            i=1
-            while i<7 and party.get(i)!=None:
-                lvltot+=party.get(i).level
-                pokect+=1
-                i+=1
-            lvlavg=round(lvltot/pokect)
-            if lvlavg>=7:
-                level=random.randint(lvlavg-5,lvlavg+5)
-            else:
-                level=random.randint(2,lvlavg+5)
+            poke=dex[choice[0]-1]
+            level=random.randint(choice[1],choice[2])
             party.p7 = pokeparty.pokemon(species=poke, level=level)
             party.p1.part = True
         fight=self.bot.get_cog("Fight")
@@ -233,7 +227,105 @@ class Pokemon(commands.Cog):
         else:
             await ctx.send("Already in battle", files=[result[1], result[2]], embed=result[0])
         with open("json/parties/"+str(ctx.author.id)+".json", "w") as f_obj:
-            json.dump(party.recon(), f_obj, indent=4)
-
+            json.dump(party.export(), f_obj, indent=4)
+    
+    @commands.command(brief="Accesses PC", description="Accesses PC", help="Brings up a popup and waits for a response for the slot. Can't be used in battle")
+    @commands.check(in_fight)
+    async def pc(self, ctx):
+        with open("json/parties/"+str(ctx.author.id)+".json") as f_obj:
+            party = pokeparty.party(json.load(f_obj))
+        with open("json/pc/"+str(ctx.author.id)+".json") as f_obj:
+            pc = pokeparty.pc(json.load(f_obj))
+        embed=discord.Embed(title="Monte's PC", color=discord.Color.blue())
+        embed.add_field(name="1. Withdraw Pokemon", value="\u200B", inline=False)
+        embed.add_field(name="2. Deposit Pokemon", value="\u200B", inline=False)
+        embed.add_field(name="3. Release Pokemon", value="\u200B", inline=False)
+        embed.add_field(name="4. Change box", value="\u200B", inline=False)
+        def yes(m):
+            return m.author == ctx.author
+        await ctx.send("PC accessed (10 second timeout)", embed=embed)
+        try:
+            answer1 = await self.bot.wait_for("message", check=yes, timeout=10.0)
+        except asyncio.TimeoutError:
+            return await ctx.send("Timed out")
+        answer1 = int(answer1.content)
+        if answer1 == 1: title = "Withdraw Pokemon"
+        elif answer1 == 2: title = "Deposit Pokemon"
+        elif answer1 == 3: title = "Release Pokemon"
+        elif answer1 == 4: title = "Change box"
+        else: return await ctx.send("Invalid Input")
+        embed2=discord.Embed(title=title, color=discord.Color.blue())
+        if answer1 == 4:
+            i = 1
+            while i <= 8:
+                tbox = pc.get_box(i)
+                embed2.add_field(name=str(i)+" - "+str(len(tbox))+"/30", value="\u200B", inline=False)
+                i+=1
+        elif answer1 != 2:
+            tbox = pc.get_box()
+            i = 0
+            string = ""
+            while i < len(tbox):
+                p = pokeparty.pokemon(tbox[i])
+                string += str(i+1)+" "+p.species+" Lvl:"+str(p.level)+"\n"
+                i+=1
+            embed2.add_field(name="\u200B", value=string, inline=False)
+        else:
+            i = 1
+            while i < 7:
+                p = party.get(i)
+                if p == None:
+                    embed2.add_field(name=str(i), value="Empty", inline=False)
+                elif p != None:
+                    embed2.add_field(name=str(i), value=p.species+" Lvl: "+str(p.level), inline=False)
+                i+=1
+        await ctx.send("Make your selection (30 second timeout)", embed=embed2)
+        try:
+            answer2 = await self.bot.wait_for("message", check=yes, timeout=30.0)
+        except asyncio.TimeoutError:
+            return await ctx.send("Timed out")
+        answer2 = int(answer2.content)
+        if answer1 == 1:
+            if answer2 > len(tbox) or answer2 < 1:
+                return await ctx.send("Invalid Input")
+            if party.p6 != None:
+                return await ctx.send("Party Full")
+            poke = pokeparty.pokemon(pc.withdraw(answer2))
+            slot = party.add(poke)
+            await ctx.send(poke.species+" withdrawn to slot "+str(slot))
+        elif answer1 == 2:
+            tbox = pc.get_box()
+            if len(tbox) == 30:
+                return await ctx.send("Box is Full")
+            if party.get(answer2) == None:
+                return await ctx.send("Invalid Input")
+            poke = party.remove(answer2)
+            pc.deposit(poke.export())
+            await ctx.send(poke.species+" deposited to box "+str(pc.main))
+        elif answer1 == 3:
+            if answer2 > len(tbox):
+                return await ctx.send("Invalid Input")
+            await ctx.send("Are you sure you want to delete "+tbox[answer2-1]["species"]+"? (y/n)")
+            try:
+                answer = await self.bot.wait_for("message", check=yes, timeout=10.0)
+            except asyncio.TimeoutError:
+                return await ctx.send("Timed out")
+            if answer.content.lower() == "y" or answer.content.lower() == "yes":
+                pc.release(answer2)
+                await ctx.send("Pokemon released")
+            elif answer.content.lower() == "n" or answer.content.lower() == "no":
+                return await ctx.send("Release cancelled")
+            else:
+                return await ctx.send("Invalid answer")
+        elif answer1 == 4:
+            if answer2 > 8:
+                return await ctx.send("Invalid Input")
+            pc.main = answer2
+            await ctx.send("Box was changed to "+str(answer2))
+        with open("json/parties/"+str(ctx.author.id)+".json", "w") as f_obj:
+            json.dump(party.export(), f_obj, indent=4)
+        with open("json/pc/"+str(ctx.author.id)+".json", "w") as f_obj:
+            json.dump(pc.export(), f_obj)
+    
 def setup(bot):
     bot.add_cog(Pokemon(bot))
